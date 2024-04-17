b2b1d4fc6db705676400bdbd7d6524c478752718
kunaldec022002 <kunalingale72@gmail.com>
2024-04-17 18:10:57 +0530

    add library and completed project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 
+import { useState } from 'react';
 import './App.css';
+import movieTrailer from 'movie-trailer';
+import ReactPlayer from 'react-player';
 
 function App() {
+
+  // setting up the initial state using
+  //react hook 'usestate
+
+  const [video , setVideo] = useState('inception');
+  const [videoURL, setVideoURL] = useState('http://youtu.be/sa9l-dTv9Gk');
+
+  //A function to fetch the required URL
+  //and storing it inside the videoURL state variable
+
+
+  function handleSearch(){
+    movieTrailer(video).then((res)=> {
+      setVideoURL(res);
+    });
+  }
   return (
+
     <div>
       <h1>Movie-Search-App</h1>
       
@@ -12,14 +32,22 @@ function App() {
           Search for any movies/shows: { " "}
         </label>
 
-        <input type='text'/>
+        <input type='text' onChange={(e) => {
+          setVideo(e.target.value)
+        }}/>
 
-        <button>Search</button>
+        <button onClick={()=> {
+          handleSearch()
+        }}>Search</button>
 
       </div>
+      
+
+      <ReactPlayer url={videoURL} controls={true}/>
   
     </div>
   );
 }
+
 
 export default App;
