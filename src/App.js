6cb0567242c473c02e14e50f4141a47d87efefd2
kunaldec022002 <kunalingale72@gmail.com>
2024-04-17 17:49:55 +0530

    create user interface

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
   return (
     <div>
       <h1>Movie-Search-App</h1>
+      
+      <div className='search-box'>
+
+        <label>
+          Search for any movies/shows: { " "}
+        </label>
+
+        <input type='text'/>
+
+        <button>Search</button>
+
+      </div>
   
     </div>
   );
